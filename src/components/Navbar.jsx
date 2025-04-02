645429dfbb26c99a0004aee20fe109cc6ef23fe9
AvendanoisPepe <jsavendano62@misena.edu.co>
2025-04-01 19:43:21 -0500

    aprendieno responsive en esta amdre

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,35 +3,45 @@ import "./navbar.scss"; // Importa el archivo SASS
 
 function Navbar() {
   return (
-    <header className="w-full flex justify-between items-center">
-      <div className="contentLogo w-[20%] flex items-center justify-center gap-4">
-        <span className="logo">
-          JS
-        </span>
+    <header className="w-full flex justify-between items-center flex-wrap p-4 2xl:p-6">
+      <div className="contentLogo md:w-[20%] flex items-center justify-center gap-4 mb-4 md:mb-0">
+        <span className="logo">JS</span>
         <p className="nombre">Avendaño</p>
       </div>
-      <nav className="menu w-[50%] flex justify-center items-center gap-6">
-        <Link to="/" className="transition ">
+      <nav className="menu w-full md:w-[50%] flex justify-center items-center gap-6">
+        <Link to="/" className="transition text-xs lg:text-lg 2xl:text-2xl">
           Inicio
         </Link>
-        <Link to="/about" className="transition">
+        <Link
+          to="/about"
+          className="transition text-xs lg:text-lg 2xl:text-2xl"
+        >
           Sobre mí
         </Link>
-        <Link to="/projects" className="transition">
+        <Link
+          to="/projects"
+          className="transition text-xs lg:text-lg 2xl:text-2xl"
+        >
           Proyectos
         </Link>
-        <Link to="/contact" className="transition">
+        <Link
+          to="/contact"
+          className="transition text-xs lg:text-lg 2xl:text-2xl"
+        >
           Estudios
         </Link>
-        <Link to="/contact" className="transition">
+        <Link
+          to="/contact"
+          className="transition text-xs lg:text-lg 2xl:text-2xl"
+        >
           Referencias
         </Link>
       </nav>
-      <div className="botones flex justify-center items-center gap-4 w-[30%] ">
-        <button className="botonesOne w-[46%] transition">
+      <div className="botones w-full md:w-[30%] flex justify-center items-center gap-4 mt-4 md:mt-0">
+        <button className="botonesOne w-[46%] transition text-xs md:text-sm">
           CV
         </button>
-        <button className="botonesTwo w-[46%] transition">
+        <button className="botonesTwo w-[46%] transition text-xs md:text-sm">
           Contacto
         </button>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,12 @@ function Home() {
   return (
     <div class="text-black p-4">
       ¡Hola, Tailwind está funcionando!
+      DESCRIPCION
+      COMENTARIOS
+      PLAN INICIAL
+      OBJETIVO ACTUAL
+      FUERA DE LA SILLA
+
     </div>
   );
 }
