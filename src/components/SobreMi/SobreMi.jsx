bf5025c883becca8a6e586317a8d82f354e5b19a
AvendanoisPepe <jsavendano62@misena.edu.co>
2025-05-11 11:23:35 -0500

    idea sin avanzce

--- src/components/SobreMi/SobreMi.jsx
+++ src/components/SobreMi/SobreMi.jsx
@@ -1,8 +1,17 @@
 import './SobreMi.scss';
+import circuito from "../../assets/imagenes/tecnolo.png"
+import circuito2 from "../../assets/imagenes/tecnolo2.png";
 const SobreMi = () => {
+    const fondoPrimero = {
+        backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url(${circuito})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center bottom",
+        backgroundSize: "80%",
+    };
+    
     return (
         <section className="SobreMi w-full flex flex-col items-center justify-between gap-6 md:flex-row md:p-10">
-            <div className="primero w-[32%] p-6 flex items-center justify-center flex-wrap">
+            <div style={fondoPrimero} className="primero w-[32%] p-6 flex items-center justify-center flex-wrap min-h-[80vh]">
                 <h3>Inicios</h3>
                 <p>
                     Empece desde el 2019 cuando no tenia ninguna idea de lo que queria hacer, se me dio la oportunidad de estudiar
@@ -10,7 +19,7 @@ const SobreMi = () => {
                     necesidad de completar el desafio me llevaron al dia de hoy, me dedico de forma muy satisfactoria a mi trabajo.
                 </p>
             </div>
-            <div className="segundo w-[32%] p-6 flex items-center justify-center flex-wrap">
+            <div className="segundo w-[32%] p-6 flex items-center justify-center flex-wrap min-h-[80vh]">
                 <h3>Objetivo actual</h3>
                 <p>
                     En lo personal me apasiona mucho mas el desarrollo forntend que el backend, mas igual combinarlo le da el toque de innovacion, ya que
@@ -18,7 +27,7 @@ const SobreMi = () => {
                     por aprender y hacer que mis desarrollos y los clientes se sientan confiados en que su desarrollo tendra la mejor calidad posible.
                 </p>
             </div>
-            <div className="tercero w-[32%] p-6 flex items-center justify-center flex-wrap">
+            <div className="tercero w-[32%] p-6 flex items-center justify-center flex-wrap min-h-[80vh]">
                 <h3>Pasatiempos</h3>
                 <p>
                     Como programar requiere estar sentado mucho tiempo, mi tiempo libre debe ser muy motriz, en este caso el deporte y el ejerciio es la
