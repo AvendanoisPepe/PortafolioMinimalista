ca855303f8b5a299ed4f6fd83d6184596d937191
AvendanoisPepe <jsavendano62@misena.edu.co>
2025-06-04 00:37:24 -0500

    imaganes version nueva colpatrai

--- src/components/Estudios/Bases/ColpatriaV2.jsx
+++ src/components/Estudios/Bases/ColpatriaV2.jsx
@@ -8,19 +8,25 @@ import {
 import { BsFileEarmarkPdfFill } from "react-icons/bs";
 
 import col1 from "../../../assets/imagenes/trabajos/Colpatria/V2/col1.png";
-import col2 from "../../../assets/imagenes/trabajos/col2.png";
-import col3 from "../../../assets/imagenes/trabajos/Colpatria/macro1.png";
-import col4 from "../../../assets/imagenes/trabajos/Colpatria/check1.png";
-import col5 from "../../../assets/imagenes/trabajos/Colpatria/check2.png";
-import col6 from "../../../assets/imagenes/trabajos/Colpatria/check3.png";
-import col7 from "../../../assets/imagenes/trabajos/Colpatria/macro2.png";
-import col8 from "../../../assets/imagenes/trabajos/Colpatria/notas1.png";
-import col9 from "../../../assets/imagenes/trabajos/Colpatria/notas2.png";
-import col10 from "../../../assets/imagenes/trabajos/Colpatria/notas3.png";
-import col11 from "../../../assets/imagenes/trabajos/Colpatria/tipificador1.png";
-import col12 from "../../../assets/imagenes/trabajos/Colpatria/tipificador2.png";
+import col2 from "../../../assets/imagenes/trabajos/Colpatria/V2/col2.png";
+import col3 from "../../../assets/imagenes/trabajos/Colpatria/V2/col6.png";
+import col4 from "../../../assets/imagenes/trabajos/Colpatria/V2/col7.png";
+import col5 from "../../../assets/imagenes/trabajos/Colpatria/V2/col8.png";
+import col6 from "../../../assets/imagenes/trabajos/Colpatria/V2/col9.png";
+import col7 from "../../../assets/imagenes/trabajos/Colpatria/V2/col10.png";
+import col8 from "../../../assets/imagenes/trabajos/Colpatria/V2/col11.png";
+import col9 from "../../../assets/imagenes/trabajos/Colpatria/V2/col12.png";
+import col10 from "../../../assets/imagenes/trabajos/Colpatria/V2/col13.png";
+import col11 from "../../../assets/imagenes/trabajos/Colpatria/V2/col14.png";
+import col12 from "../../../assets/imagenes/trabajos/Colpatria/V2/col15.png";
+import col13 from "../../../assets/imagenes/trabajos/Colpatria/V2/col16.png";
+import col14 from "../../../assets/imagenes/trabajos/Colpatria/V2/col17.png";
+import col15 from "../../../assets/imagenes/trabajos/Colpatria/V2/col3.png";
+import col16 from "../../../assets/imagenes/trabajos/Colpatria/V2/col4.png";
+import col17 from "../../../assets/imagenes/trabajos/Colpatria/V2/col5.png";
+import col18 from "../../../assets/imagenes/trabajos/Colpatria/V2/col18.png";
 
-const images = [col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12];
+const images = [col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16, col17, col18];
 
 const ColpatriaV1 = {
     title: "Web Training Colpatria Sac",
@@ -34,7 +40,7 @@ const ColpatriaV1 = {
         { name: "Sass", icon: "🎨" },
         { name: "WebPack", icon: "🚀" },
     ],
-    images: [images[0], images[1], images[2]],
+    images: [images[0], images[1], images[2], images[3], images[4]],
     projectIcon: "🛒",
     actionIcon: <FaExternalLinkAlt />,
     slideDescription:
@@ -50,7 +56,7 @@ const ColpatriaV1 = {
             icon: <FaCheckSquare />,
             description:
                 "Compilacion de procesos y paso a paso para optimizar flujos de trabajo repetitivos. Para minimizar tareas manuales y aumentar la eficiencia. Incluyendo todo tipo de contenido, imagenes, Pdfs, Listas desplegables, Recopilacion de datos, entre otras cosas.",
-            images: [images[3], images[4], images[5]],
+            images: [images[3], images[5], images[6], images[7], images[8]],
         },
         {
             id: "macros",
@@ -58,15 +64,15 @@ const ColpatriaV1 = {
             icon: <FaCog />,
             description:
                 "Buscador basado en un codigo macro, que cuenta con contenido diverso al encontrar coincidencia para informar en todo aspecto al usuario, al igual que mostrarle que conjunto de datos debe ingresar para completar el proceso.",
-            images: [images[2], images[6]],
+            images: [images[2], images[9], images[10]],
         },
         {
-        id: "notes",
-        name: "Generador de Notas",
-        icon: <FaStickyNote />,
-        description:
-            "Sistema avanzado para crear y gestionar notas con formato enriquecido, donde se verifica campo por campo con sus diferentes validaciones, recolectando datos para formar la nota deseada.",
-            images: [images[7], images[8], images[9]],
+            id: "notes",
+            name: "Generador de Notas",
+            icon: <FaStickyNote />,
+            description:
+                "Sistema avanzado para crear y gestionar notas con formato enriquecido, donde se verifica campo por campo con sus diferentes validaciones, recolectando datos para formar la nota deseada. (El sistema esta enlazado con las macros)",
+                images: [images[10], images[11], images[12], images[13]],
         },
         {
         id: "documentacion",
@@ -74,7 +80,7 @@ const ColpatriaV1 = {
         icon: <BsFileEarmarkPdfFill />,
         description:
             "Sistema de consulta de documentación para todo tipo de formato PDF, WORD, EXCEL, POWER POINT, etc.",
-        images: [images[0]],
+        images: [images[1], images[14], images[15], images[16]],
         },
         {
         id: "tipificacion",
@@ -83,7 +89,7 @@ const ColpatriaV1 = {
         description:
             "Sistema de decision donde el usuario debe seguir las ramificaciones dadas por el sistemas para hayar la respuesta correcta al caso de uso en este caso, de igual forma como es un ayudante se debe medir el tiempo de respuesta del mismo.",
         images: [
-            images[10], images[11]
+            images[4], images[17]
         ],
         },
     ],
